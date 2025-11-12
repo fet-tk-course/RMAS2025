3333d80e86abebe7b81b439ceda3535f88585837
Alma Secerbegovic <alma.secerbegovic@fet.ba>
2025-11-12 16:10:23 +0100

    added P7 starter+solution

---
+++ lectures/P7/PositiveQuotesStarter/app/src/main/java/com/example/positivequotesstarter/data/DataSource.kt
@@ -0,0 +1,23 @@
+package com.example.positivequotesstarter.data
+
+//import com.example.positivequotesstarter.R
+//import com.example.positivequotesstarter.model.Quote
+
+/**
+ * [Datasource] generates a list of [Quotes]
+ */
+class Datasource() {
+//    fun loadQuotes(): List<Quote> {
+//        return listOf<Quote>(
+//            Quote(R.string.affirmation1, R.drawable.image1),
+//            Quote(R.string.affirmation2, R.drawable.image2),
+//            Quote(R.string.affirmation3, R.drawable.image3),
+//            Quote(R.string.affirmation4, R.drawable.image4),
+//            Quote(R.string.affirmation5, R.drawable.image5),
+//            Quote(R.string.affirmation6, R.drawable.image6),
+//            Quote(R.string.affirmation7, R.drawable.image7),
+//            Quote(R.string.affirmation8, R.drawable.image8),
+//            Quote(R.string.affirmation9, R.drawable.image9),
+//            Quote(R.string.affirmation10, R.drawable.image10))
+//    }
+}
